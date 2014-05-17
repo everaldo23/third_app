4f820887bfff3b82c943316ea32500d499f394b7
Syahril <everaldo@umich.edu>
2014-05-17 14:33:18 -0400

    Passed the second test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,12 @@ describe "StaticPages" do
       visit'/static_pages/home'
       expect(page) .to have_content('Third App')
     end
-  end  
+  end
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit'/static_pages/help' 
+      expect(page) .to have_content('Help')
+    end
+  end
 end
