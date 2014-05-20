91b510b64512c392ca83e507a909aab99e37ed8f
Syahril <everaldo@umich.edu>
2014-05-20 18:03:51 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Syahril"
+    email                 "everaldo@umich.edu"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+end
