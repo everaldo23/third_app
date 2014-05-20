f866ab429e900aa1db2cb98bc56703e081a84a40
Syahril <everaldo@umich.edu>
2014-05-20 18:23:41 -0400

    Reinserted SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ ThirdApp::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-  #config.force_ssl = true
+  config.force_ssl = true
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
